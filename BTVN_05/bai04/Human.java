06abfaacd1e6165e6428aa7ef9ad5b9b710462cf
Vũ Văn Doan <55150896+vuvandoancnttk13haui@users.noreply.github.com>
2019-11-08 18:51:15 +0700

    em sua btvn

--- BTVN_05/bai04/Human.java
+++ BTVN_05/bai04/Human.java
@@ -1,6 +1,5 @@
 package bai04;
 
-
 public class Human {
 	// bài 4
 	private String name;
@@ -11,36 +10,25 @@ public class Human {
 
 	public void attack(Human x) {
 		dame = (int) (Math.random() * 20);
-		x.dame = (int) (Math.random() * 15);
+		x.dame = (int) (Math.random() * 20);
 		while (HP > 0 && x.getHP() > 0) {
-			for (int i = 1; i <= 4 && x.getHP() > 0; i++) {
-				x.setHP(x.getHP() - dame);
-				count++;
-				System.out.println("==============Lượt đánh thứ " + count + "==============");
-				System.out.print("HP " + name + ": " + HP);
-				System.out.println("\t==vs==\tHP " + x.getName() + ": " + x.getHP());
-				if (HP <= 0) {
-					System.out.println(name+" đã chết, "+x.getName() + " chiến thắng");
-					return;
-				} else if (x.getHP() <= 0) {
-					System.out.println(x.getName()+" đã chết, "+name + " chiến thắng");
-					return;
-				}
+			x.setHP(x.getHP() - dame);
+			count++;
+			System.out.println("==============Lượt đánh thứ " + count + "==============");
+			System.out.print("HP " + name + ": " + HP);
+			System.out.println("\t==vs==\tHP " + x.getName() + ": " + x.getHP());
+			if (x.getHP() <= 0) {
+				System.out.println(x.getName() + " đã chết, " + name + " chiến thắng");
+				return;
 			}
-
-			for (int i = 1; i <= 5 && HP > 0; i++) {
-				HP -= x.dame;
-				count++;
-				System.out.println("==============Lượt đánh thứ " + count + "==============");
-				System.out.print("HP " + name + ": " + HP);
-				System.out.println("\t==vs==\tHP " + x.getName() + ": " + x.getHP());
-				if (HP <= 0) {
-					System.out.println(name+" đã chết, "+x.getName() + " chiến thắng");
-					return;
-				} else if (x.getHP() <= 0) {
-					System.out.println(x.getName()+" đã chết, "+name + " chiến thắng");
-					return;
-				}
+			HP -= x.dame;
+			count++;
+			System.out.println("==============Lượt đánh thứ " + count + "==============");
+			System.out.print("HP " + name + ": " + HP);
+			System.out.println("\t==vs==\tHP " + x.getName() + ": " + x.getHP());
+			if (HP <= 0) {
+				System.out.println(name + " đã chết, " + x.getName() + " chiến thắng");
+				return;
 			}
 		}
 
